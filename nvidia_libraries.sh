7d422d7317c07e74e1aefe5d779efd276ca9a260
nemo <git@nemo.ikkoku.de>
2016-02-29 17:41:05 +0100

    Update to current NVIDIA driver

--- nvidia_libraries.sh
+++ nvidia_libraries.sh
@@ -3,13 +3,13 @@
 # NVIDIA_ARCH controls register size. NVIDIA_ARCH=32 for 32 bit
 # libraries, every other value (including none) for 64 bit libraries.
 
-NVIDIA_VERSION=${NVIDIA_VERSION:-346}
+NVIDIA_VERSION=${NVIDIA_VERSION:-352-updates}
 NVIDIA_PATH=""
 
 if [ "$NVIDIA_ARCH" = "32" ]; then
-	NVIDIA_PATH="/usr/lib32/nvidia-${NVIDIA_VERSION}:/usr/lib/i386-linux-gnu/primus"
+	NVIDIA_PATH="/usr/lib32/nvidia-${NVIDIA_VERSION}:/usr/lib32/nvidia-${NVIDIA_VERSION}/tls:/usr/lib/i386-linux-gnu/primus"
 else
-	NVIDIA_PATH="/usr/lib/nvidia-${NVIDIA_VERSION}:/usr/lib/x86_64-linux-gnu/primus"
+	NVIDIA_PATH="/usr/lib/nvidia-${NVIDIA_VERSION}:/usr/lib/nvidia-${NVIDIA_VERSION}/tls:/usr/lib/x86_64-linux-gnu/primus"
 fi
 
 export LD_LIBRARY_PATH="${NVIDIA_PATH}:${LD_LIBRARY_PATH}"
